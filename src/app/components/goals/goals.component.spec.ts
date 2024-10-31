b2e92a15a8fa75d8869f52d31207782760d072b7
Taylor Cheung <tlc200003@utdallas.edu>
2024-10-31 16:49:05 -0500

    git

--- src/app/components/goals/goals.component.spec.ts
+++ src/app/components/goals/goals.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { GoalsComponent } from './goals.component';
 
 describe('GoalsComponent', () => {
