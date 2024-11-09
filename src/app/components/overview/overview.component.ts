ae8952da3f6337b512bccd1732b6576bf90ccc17
JudYang4319 <157545963+JudYang4319@users.noreply.github.com>
2024-11-09 12:18:43 -0600

    Implement a Working Quiz

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -2,13 +2,21 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
 
 import { Chart, ChartConfiguration, ChartData, ChartType, registerables } from 'chart.js';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
 Chart.register(...registerables);
+
+// For Daily Question
+interface Question {
+  question: string;
+  options: string[];
+  answer: string;
+}
 
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
@@ -16,6 +24,33 @@ export class OverviewComponent implements OnInit, AfterViewInit {
   @ViewChild('pieChart') private chartRef!: ElementRef<HTMLCanvasElement>;
   chart: Chart | undefined;
   isQuizOpen: boolean = false;
+  isAnswered: boolean = false; // Tracks if the question has been answered
+
+  // Array of questions
+  questions: Question[] = [
+    {
+      question: 'What is the capital of France?',
+      options: ['Berlin', 'Marid', 'Paris', 'Rome'],
+      answer: 'Paris'
+    },
+    {
+      question: 'What is 2 + 2?',
+      options: ['3', '4', '5', '6'],
+      answer: '4'
+    },
+    {
+      question: 'Largest ocean on Earth?',
+      options: ['Atlantic', 'Indian', 'Arctic', 'Pacific'],
+      answer: 'Pacific'
+    }
+  ];
+
+  currentQuestion: Question | null = null;
+  selectedOption: string | null = null;
+
+
+
+  // Functions
 
   ngOnInit(): void {
     // Initialization logic if needed
@@ -28,11 +63,30 @@ export class OverviewComponent implements OnInit, AfterViewInit {
   openQuiz(): void
   {
     this.isQuizOpen = true;
+    this.isAnswered = false; // Resets the answered state
   }
   
   closeQuiz(): void
   {
+    this.isAnswered = true;
     this.isQuizOpen = false;
+    this.currentQuestion = null; // Clear the current question
+  }
+
+  loadRandomQuestion() {
+    this.openQuiz();
+    const randomIndex = Math.floor(Math.random() * this.questions.length);
+    this.currentQuestion = this.questions[randomIndex];
+    this.selectedOption = null; // Resets selected option
+  }
+
+  submitAnswer() {
+    if(this.selectedOption === this.currentQuestion?.answer) {
+      alert('Correct! You earned 50 points');
+      this.closeQuiz();
+    } else {
+      alert('Wrong answer. Try again.');
+    }
   }
 
   createChart(): void {
