04906a9309525ccbf51ab5663b1d274527f2d341
harryytran <tranh1025@gmail.com>
2024-12-02 20:10:38 -0600

    fixes images, adds search functionality for friends, separates color picker and icons for a less overwhelming design, adds confirmation for icons/colors picked

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,32 +18,48 @@ import { ViewGuildComponent } from './components/viewguild/viewguild.component';
 import { DebtRepaymentComponent } from './debt-repayment/debt-repayment.component';
 import { CreateAccountComponent } from './components/createaccount/createaccount.component';
 import { MyFriendsComponent } from './components/myfriends/myfriends.component';
+import { CreateAccountComponent } from './components/createaccount/createaccount.component';
+import { MyFriendsComponent } from './components/myfriends/myfriends.component';
 
 
 export const routes: Routes = [
-  { path: 'goals', component: GoalsComponent, data: { title: 'Goals' } },
-  { path: 'news', component: NewsComponent, data: { title: 'News' } },
-  { path: 'overview', component: OverviewComponent, data: { title: 'Overview' } },
-  { path: 'sidebar', component: SidebarComponent, data: { title: 'Sidebar' } },
-  { path: 'glossary', component: GlossaryComponent, data: { title: 'Glossary' } },
-  { path: 'debt-repayment', component: DebtRepaymentComponent, data: { title: 'Debt Repayment' } },
-  { path: 'profile', component: SettingsComponent, data: { title: 'Profile' } },
-  { path: 'transactions', component: TransactionsComponent, data: { title: 'Transactions' } },
-  { path: 'friends', component: FriendlistComponent, data: { title: 'Friends' } },
-  { path: 'login', component: LoginComponent, data: { title: 'Login' } },
-  { path: 'guildstart', component: GuildstartComponent, data: { title: 'Guild Start' } },
-  { path: 'create', component: CreateComponent, data: { title: 'Create' } },
-  { path: 'flagcreator', component: FlagcreatorComponent, data: { title: 'Flag Creator' } },
-  { path: 'guildcreated', component: GuildcreatedComponent, data: { title: 'Guild Created' } },
-  { path: 'myfriends', component: MyFriendsComponent, data: { title: 'My Friends' }  },
-  { path: 'viewguild', component: ViewGuildComponent, data: { title: 'View Guild' } },
-  { path: 'friend-profile/:id', component: FriendProfileComponent, data: { title: 'Friend Profile' } },
+  { path: 'goals', component: GoalsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'glossary', component: GlossaryComponent },
+  { path: 'debt-repayment', component: DebtRepaymentComponent },
+  { path: 'profile', component: SettingsComponent },
+  { path: 'glossary', component: GlossaryComponent },
+  { path: 'transactions', component: TransactionsComponent }, // Correct route
+  { path: 'friends', component: FriendlistComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'guildstart', component: GuildstartComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'flagcreator', component: FlagcreatorComponent },
+  { path: 'guildcreated', component: GuildcreatedComponent },
+  { path: 'viewguild', component: ViewGuildComponent },
+  { path: 'guildstart', component: GuildstartComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'flagcreator', component: FlagcreatorComponent },
+  { path: 'guildcreated', component: GuildcreatedComponent },
+  { path: 'createaccount', component: CreateAccountComponent },
+  { path: 'myfriends', component: MyFriendsComponent },
+  { path: 'viewguild', component: ViewGuildComponent },
+  { path: 'overview', component: OverviewComponent, canActivate: [AuthGuard] },
+  { path: 'friend-profile/:id', component: FriendProfileComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' },
 
 
 
+
+
+
   { path: '**', redirectTo: '/login' },
+
+
+
 
 
 
