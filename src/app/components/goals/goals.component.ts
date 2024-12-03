7fc799530c163fd91290879ebf43fa6d6824dfe2
Taylor Cheung <tlc200003@utdallas.edu>
2024-12-03 15:13:42 -0600

    adds feedback changes for taylor components

--- src/app/components/goals/goals.component.ts
+++ src/app/components/goals/goals.component.ts
@@ -159,6 +159,7 @@ export class GoalsComponent {
       console.log("Total cost cannot be a negative number.");
       alert("Total cost cannot be a negative number.");
       return; // Exit if total cost is negative
+
     }
 
     if (!this.goalTitle || !this.totalCost || !this.startDate || !this.endDate || !this.paymentSchedule) {
@@ -232,6 +233,9 @@ export class GoalsComponent {
     this.goals.splice(index, 1); // Remove the goal from the array
     localStorage.setItem('userGoals', JSON.stringify(this.goals)); // Update local storage
     this.goalToRemove = null; // Reset the prompt
+    this.goals.splice(index, 1);
+    localStorage.setItem('userGoals', JSON.stringify(this.goals));
+    this.goalToRemove = null;
   }
 
   cancelRemoveGoal(): void {
